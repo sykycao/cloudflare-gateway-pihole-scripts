5113b18f6062bdb0fd3a96411fc1f0a72c5a3794
Cao Ky <sykycao@gmail.com>
2023-08-11 21:05:08 +0700

    update script

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -7,6 +7,8 @@ touch input.csv
 urls=(
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
+  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 )
 
 # loop through the urls and download each file with curl
